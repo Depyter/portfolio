1ef3d90ab1165684070d7063b6fd0c972fb5870b
Depyter <acabalharleyvan@gmail.com>
2024-01-19 14:15:55 +0800

    Update grid layout and image sizes in index.html and tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,9 @@ module.exports = {
   plugins: [
     require('tailwindcss-animated')
   ],
+  theme: {
+    gridTemplateColumns:{
+      '69': 'repeat(auto-fill, minmax(360px, 1fr))',
+    }
+  }
 };
